02f8eee17e550b0a385b33026b400ef370af476d
cha <61283384+cha-afk@users.noreply.github.com>
2020-09-17 23:20:04 +0100

    Update bincheck.py

--- bincheck.py
+++ bincheck.py
@@ -1,25 +1,31 @@
-import urllib.request, colorama, os, ctypes
+#!/usr/bin/python
+import requests,os
 from colorama import Fore
-
-os.system('cls')
-os.system("title [Bin checker] By Nayfer")
-
-
-
+if os.name=='nt':
+ #WINDOWS
+ os.system('cls')
+ os.system("title [Bin checker] By Nayfer")
+else:
+ #LINUX
+ os.system('clear')
 print(f'''{Fore.BLUE}
-  ____  _          _____ _               _             
+  __  _          ___ _               _             
  |  _ \(_)        / ____| |             | |            
- | |_) |_ _ __   | |    | |__   ___  ___| | _____ _ __ 
+ | |_) |_ _ _   | |    | |_   _  __| | ____ _ __ 
  |  _ <| | '_ \  | |    | '_ \ / _ \/ __| |/ / _ \ '__|
  | |_) | | | | | | |____| | | |  __/ (__|   <  __/ |   
  |____/|_|_| |_|  \_____|_| |_|\___|\___|_|\_\___|_|                                                                                                       
                     github.com/cha-afk {Fore.RESET}''')
-headers = {
-    'Accept-Version': '3',
-}
-
-bin = input('Your bin = ')
-a = urllib.request.urlretrieve(f'https://lookup.binlist.net/'+bin, f'Bin.txt',)
-print(f'{Fore.BLUE}> {Fore.RESET}Successfully Grabbed Bin infos')
-input()
-exit()
+bin = str(input('Your bin or cc = '))
+if not(len(bin)<=6):
+ Rbin=""
+ for i in range(0,6):
+  Rbin=Rbin+bin[i]
+else:
+ Rbin=bin
+response=requests.get('https://lookup.binlist.net/'+str(Rbin)).json()
+scheme=response['scheme']
+typ=response['type']
+country=response['country']['name']
+bank=response['bank']['name']
+print('Bin info : \nscheme : {a} \ntype : {b} \nCountry : {c} \nBank : {d}'.format(a=scheme,b=typ,c=country,d=bank))
